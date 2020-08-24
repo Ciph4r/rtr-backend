92aa9abe6d55a20eff15dac1d68de0ad51928d7c
Dave.L <59891341+Ciph4r@users.noreply.github.com>
2020-08-24 00:49:45 -0400

    added auto randm time for random price for stocks

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const PORT2 = process.env.PORT2 || 4000
 const http = require("http");
 const socketIo = require("socket.io");
 const History = require('./models/History')
-
+const Stocks = require('./models/Stocks')
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const stockRouter = require('./routes/stocks')
@@ -47,6 +47,9 @@ mongoose
 
 // app.use(passport.initialize());
 // app.use(passport.session());
+
+
+
 
 
 app.use(logger('dev'));
@@ -115,5 +118,43 @@ server.listen(PORT2, () => console.log(`Listening on port ${PORT2}`))
 app.listen(PORT , () => {
   log(`listening to ${PORT}`)
 })
+
+const priceRNGUpdate =  () => {
+
+  const rngPrice = (price) => {
+  let rng = Math.ceil(Math.random() * 2)
+    if (rng === 1){
+      rng = Math.ceil(Math.random() * 3)
+      return price -= (rng/100) * price
+    }else{
+      rng = Math.ceil(Math.random() * 3)
+      return price += (rng/100) * price
+    }
+  }
+
+
+
+  Stocks.find().then((stocks) => {
+    stocks.forEach((stock) => {
+      stock.price = rngPrice(stock.price).toFixed(2)
+      stock.history.push({
+        time: Date.now(),
+        price: stock.price
+    })
+      stock.save()
+    })
+  })
+}
+
+(function loop() {
+  const rngTime = Math.ceil(Math.random() * (900000 - 300000)) + 300000
+  console.log(rngTime)
+  setTimeout(function() {
+          priceRNGUpdate();
+          loop();  
+  }, rngTime);
+}());
+
+
 
 module.exports = app;
